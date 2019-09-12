edd5d71e057a157cabaaece9613d3f359160e7a8
Alexander Pavlovets <olpav@ciklum.com>
2019-09-12 14:58:25 +0300

    wait time reduced

--- specs/one.ts
+++ specs/one.ts
@@ -3,7 +3,7 @@ import { browser, $, $$, element, by, ExpectedConditions as ES } from 'protracto
 describe('First suite', () => {
     it('One is equal to one', async function () {
         await browser.get('')
-        await browser.sleep(60000)
+        await browser.sleep(10000)
         expect(1).toBe(1, '1 should be equal to 1, smth went really wrong')
     })
 })
